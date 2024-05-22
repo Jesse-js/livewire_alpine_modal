bda4d627885b304841d75a75ed1eef0841abf521
Jesse-js <jesse-jjs@hotmail.com>
2024-05-22 19:57:39 -0300

    feat: adds close modal functionality

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,6 +1,9 @@
 <div x-data="{ visible: false }" x-show="visible" x-on:open-modal.window="visible = true"
-    x-on:close-modal.window="visible = false" class="fixed z-50 inset-0">
-    <div class="fixed inset-0 bg-gray-300 opacity-400"></div>
+    x-on:close-modal.window="visible = false" x-on:keydown.escape.window="visible = false" class="fixed z-50 inset-0">
+    {{-- Background overlay --}}
+    <div @click="visible = false" class="fixed inset-0 bg-gray-300 opacity-40"></div>
+    
+    {{-- Modal content --}}
     <div class="bg-white rounded m-auto fixed inset-0 max-w-2xl" style="max-height: 500px">
         <div>
             Header
